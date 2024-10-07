5c8dad989f303f3cc15fc98a4065a14c7e149f05
Andrew J Theismann <andrew@theismannconsulting.com>
2024-10-07 13:18:46 -0400

    fix url for frontend

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,3 +1,3 @@
 export const BASE_URL: string = 'https://crazy-elysha-theismann-consulting-25d5438a.koyeb.app/';
 export const BACKEND_URL: string =
-  'https://https://silly-gwenneth-theismann-consulting-cdc943ac.koyeb.app//api/v1/';
+  'https://https://silly-gwenneth-theismann-consulting-cdc943ac.koyeb.app/api/v1/';
