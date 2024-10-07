cec11061846e9c5a6c6a6b51674c44e3a3c21862
Andrew J Theismann <andrew@theismannconsulting.com>
2024-10-07 13:04:50 -0400

    change backend url

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,3 +1,3 @@
 export const BASE_URL: string = 'https://crazy-elysha-theismann-consulting-25d5438a.koyeb.app/';
 export const BACKEND_URL: string =
-  'https://misleading-henrieta-theismann-consulting-0d201731.koyeb.app/api/v1/';
+  'https://https://silly-gwenneth-theismann-consulting-cdc943ac.koyeb.app//api/v1/';
